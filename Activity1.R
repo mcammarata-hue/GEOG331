8326e52da78fc03518e77ec738db84ff04729a91
mcammarata-hue <mcammarata@colgate.edu>
2025-09-08 10:38:26 -0400

    Added new script for Activity 1.

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,25 @@
 print("Hello World")
+#Calculate 6 raised to the power of 6
+6^6
+#Calculate 234 + 8
+234+8
+#Calculate 15-23
+15-23
+#name my number 
+a <- 6234
+#multiply my number by 5
+a*5
+#divide my number by 3 
+a/3
+# make a vector of numbers 
+b <- c(2395,82,2947)
+#divide all numbers by 2
+b/2
+#make a vector of numbers 
+d <- c(4573,97,403,2753)
+#divide all numbers by 3
+d/3
+#multiply all numbers by 6
+d*6
+#multiply all numbers by 2
+d*2
